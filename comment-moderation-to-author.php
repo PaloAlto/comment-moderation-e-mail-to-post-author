64532ec7bfa03a35506b8a8b3c355f3401c35fad
RavanH <RavanH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-05 18:44:22 +0000

    3.1+ compatibility
    git-svn-id: https://plugins.svn.wordpress.org/comment-moderation-e-mail-to-post-author/trunk@500689 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comment-moderation-to-author.php
+++ comment-moderation-to-author.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Comment Moderation E-mail to Post Author
+Plugin Name: Comment Moderation E-mail Only to Post Author
 Plugin URI: http://4visions.nl/en/wordpress-plugins/comment-moderation-e-mail-to-post-author/
-Description: Makes WordPress send the comment moderation notification to the actual posts author's e-mail address instead of the main site e-mail address. <strong>Happy with it? <em><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Comment%20Moderation%20E-mail%20to%20Post%20Author&item_number=0%2e1&no_shipping=0&tax=0&bn=PP%2dDonationsBF&charset=UTF%2d8&lc=us">"Tippin' me jar"</a></em> is much appreciated. Thanks! :)</strong>
-Version: 0.1
+Description: Sends the comment moderation notification *only* to the posts author's e-mail address and no longer to the main site admin e-mail address as set on Settings > General. Unless the author has no moderation rights. There are no options, just activate or deactivate. <strong>Happy with it? <em><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ravanhagen%40gmail%2ecom&item_name=Comment%20Moderation%20E-mail%20to%20Post%20Author&item_number=0%2e2&no_shipping=0&tax=0&bn=PP%2dDonationsBF&charset=UTF%2d8&lc=us">Buy me a coffee...</a></em> Thanks! :)</strong>
+Version: 0.2
 Author: RavanH
 Author URI: http://4visions.nl/
 */
@@ -15,11 +15,16 @@ if ( !function_exists('wp_notify_moderator') ) :
 function wp_notify_moderator($comment_id) {
 	global $wpdb;
 
-	if( get_option( "moderation_notify" ) == 0 )
+	if ( 0 == get_option( 'moderation_notify' ) )
 		return true;
 
-	$comment = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->comments WHERE comment_ID=%d LIMIT 1", $comment_id));
-	$post = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->posts WHERE ID=%d LIMIT 1", $comment->comment_post_ID));
+	$comment = get_comment($comment_id);
+	$post = get_post($comment->comment_post_ID);
+	$user = get_userdata( $post->post_author );
+	// Send to the administration and to the post author if the author can modify the comment.
+	$email_to = array( get_option('admin_email') );
+	if ( user_can($user->ID, 'edit_comment', $comment_id) && !empty($user->user_email) && ( get_option('admin_email') != $user->user_email) )
+		$email_to = array( $user->user_email ); // Replace admin email with author email :)
 
 	$comment_author_domain = @gethostbyaddr($comment->comment_author_IP);
 	$comments_waiting = $wpdb->get_var("SELECT count(comment_ID) FROM $wpdb->comments WHERE comment_approved = '0'");
@@ -50,7 +55,7 @@ function wp_notify_moderator($comment_id) {
 			$notify_message .= sprintf( __('Author : %1$s (IP: %2$s , %3$s)'), $comment->comment_author, $comment->comment_author_IP, $comment_author_domain ) . "\r\n";
 			$notify_message .= sprintf( __('E-mail : %s'), $comment->comment_author_email ) . "\r\n";
 			$notify_message .= sprintf( __('URL    : %s'), $comment->comment_author_url ) . "\r\n";
-			$notify_message .= sprintf( __('Whois  : http://ws.arin.net/cgi-bin/whois.pl?queryinput=%s'), $comment->comment_author_IP ) . "\r\n";
+			$notify_message .= sprintf( __('Whois  : http://whois.arin.net/rest/ip/%s'), $comment->comment_author_IP ) . "\r\n";
 			$notify_message .= __('Comment: ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
 			break;
 	}
@@ -67,21 +72,16 @@ function wp_notify_moderator($comment_id) {
 	$notify_message .= admin_url("edit-comments.php?comment_status=moderated") . "\r\n";
 
 	$subject = sprintf( __('[%1$s] Please moderate: "%2$s"'), $blogname, $post->post_title );
-
-	// and here is where we differ from the function in pluggable.php: 
-	// the To address will become the Author e-mail unless that is empty...
-	$user = get_userdata( $post->post_author );
-	$admin_email = ('' == $user->user_email) ? get_option('admin_email') : $user->user_email;
 	$message_headers = '';
 
 	$notify_message = apply_filters('comment_moderation_text', $notify_message, $comment_id);
 	$subject = apply_filters('comment_moderation_subject', $subject, $comment_id);
 	$message_headers = apply_filters('comment_moderation_headers', $message_headers);
 
-	@wp_mail($admin_email, $subject, $notify_message, $message_headers);
+	foreach ( $email_to as $email )
+		@wp_mail($email, $subject, $notify_message, $message_headers);
 
 	return true;
 }
 
 endif;
-
